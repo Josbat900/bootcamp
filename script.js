ef85588361c432993dcd93e85150bdacefa46c3c
SebasTian <josbat900@gmail.com>
2023-11-16 22:53:51 -0500

    funcion de crear un archivo

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ rl.question('¿Cómo te llamas? ', (answer) => {
 */
 
 
+const fs = require("fs") 
 
 const http = require('http');
 
@@ -23,15 +24,24 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     const nombre = req.url.split("?")[1]
     const edad = req.url.split("?")[2]
+    const crearArchivo = req.url.split("?")[3]
     console.log(nombre)
     console.log(edad)
 
     if(edad>18){
       res.end(`hola ${nombre}, eres mayor de edad y tienes ${edad}.`)
+      
     }else
     res.end(`${nombre } aun tienes ${edad}, lastimosamente no puedes acceder\n`);
-
-
+    
+    
+    if(crearArchivo == "si"){
+      fs.writeFile(`${nombre}.txt`, `${edad} tu edad es ${edad}`,()=>{
+        
+        console.log("funciona")
+      })
+      
+    }
 
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
