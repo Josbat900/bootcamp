33cf13d0fc1cd07b8fed8911ddb0ef8f4ebffec6
SebasTian <josbat900@gmail.com>
2023-11-16 21:47:48 -0500

    creando mi primer api

---
+++ nodelibreria.js
@@ -0,0 +1,10 @@
+/*
+const os = require("os")
+const os = require("path")
+console.log(os.hostname())
+
+*/
+
+const fs = require("fs")
+
+fs.writeFileSync("texto1.txt", "hola como estas bro")
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -8,4 +9,36 @@ const rl = readline.createInterface({
 rl.question('¿Cómo te llamas? ', (answer) => {
   console.log(`Hola , ${answer}!`);
   rl.close();
+});
+*/
+
+
+
+const http = require('http');
+
+const PORT = 3000;
+// request
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const nombre = req.url.split("?")[1]
+    const edad = req.url.split("?")[2]
+    console.log(nombre)
+    console.log(edad)
+
+    if(edad>18){
+      res.end(`hola ${nombre}, eres mayor de edad y tienes ${edad}.`)
+    }else
+    res.end(`${nombre } aun tienes ${edad}, lastimosamente no puedes acceder\n`);
+
+
+
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found\n');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
